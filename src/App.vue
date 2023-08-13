0eff50811983a3e97fb1b5e8dc2890adde62f4d5
valentin <valentinscramin@gmail.com>
2023-08-13 09:48:33 -0300

    clean

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
   <div>
-    <PrimeiroComponenteVue />
-    <LifeCycleVue />
-    <PessoaVue />
   </div>
 </template>
 
 <script>
-import LifeCycleVue from "./components/LifeCycle.vue";
-import PessoaVue from "./components/Pessoa.vue";
-import PrimeiroComponenteVue from "./components/PrimeiroComponente.vue";
-
 export default {
   name: "App",
-  components: {
-    PrimeiroComponenteVue,
-    LifeCycleVue,
-    PessoaVue,
-  },
 };
 </script>
-<style>
-body {
-  background-color: #333;
-  color: #FFF;
-}
-</style>
